fb4c75a18c41019198bf276b817b75546d3b51d5
Rashid <rashidul.hasan@dsinnovators.com>
2021-09-29 14:01:10 +0600

    add redux-persist

--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -1,8 +1,19 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "../Reducers/rootReducer";
 import thunkMiddleware from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+
+const persistConfig = {
+    key : 'rootReducer',
+    storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const enhancer = applyMiddleware(thunkMiddleware);
-const store = createStore(rootReducer, enhancer);
+const store = createStore(persistedReducer, enhancer);
 
+export const persistor = persistStore(store);
 export default store;
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,52 +8,55 @@ import PageTemplate from "./PageTemplate";
 import AboutUs from "./pages/AboutUs";
 import Contacts from "./pages/Contacts";
 import ErrorPage from "./pages/ErrorPage";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import store from "../Redux/Store/store";
+import store, { persistor } from "../Redux/Store/store";
 
 
 function Home() {
     return (
         <Provider store={store} >
-            <Router>
-                <Switch>
-                    <Route exact path="/" >
-                        <PageTemplate>
-                            <Products />
-                        </PageTemplate>
-                    </Route>
-                    <Route exact path="/checkout/" >
-                        <PageTemplate>
-                            <CheckoutPage />
-                        </PageTemplate>
-                    </Route>
-                    <Route path="/success/" >
-                        <PageTemplate>
-                            <PurchaseSuccess />
-                        </PageTemplate>
-                    </Route>
-                    <Route path="/about/" >
-                        <PageTemplate>
-                            <AboutUs />
-                        </PageTemplate>
-                    </Route>
-                    <Route path="/contacts/" >
-                        <PageTemplate>
-                            <Contacts />
-                        </PageTemplate>
-                    </Route>
-                    <Route path="/error/" >
-                        <PageTemplate>
-                            <ErrorPage />
-                        </PageTemplate>
-                    </Route>
-                    <Route path="*" >
-                        <PageTemplate>    
-                            <NotFoundPage />
-                        </PageTemplate>
-                    </Route>
-                </Switch>
-            </Router>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <Switch>
+                        <Route exact path="/" >
+                            <PageTemplate>
+                                <Products />
+                            </PageTemplate>
+                        </Route>
+                        <Route exact path="/checkout/" >
+                            <PageTemplate>
+                                <CheckoutPage />
+                            </PageTemplate>
+                        </Route>
+                        <Route path="/success/" >
+                            <PageTemplate>
+                                <PurchaseSuccess />
+                            </PageTemplate>
+                        </Route>
+                        <Route path="/about/" >
+                            <PageTemplate>
+                                <AboutUs />
+                            </PageTemplate>
+                        </Route>
+                        <Route path="/contacts/" >
+                            <PageTemplate>
+                                <Contacts />
+                            </PageTemplate>
+                        </Route>
+                        <Route path="/error/" >
+                            <PageTemplate>
+                                <ErrorPage />
+                            </PageTemplate>
+                        </Route>
+                        <Route path="*" >
+                            <PageTemplate>    
+                                <NotFoundPage />
+                            </PageTemplate>
+                        </Route>
+                    </Switch>
+                </Router>
+            </PersistGate>
         </Provider>
     )
 }
